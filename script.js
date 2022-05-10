7abc6e4f40862df49426a9f92f86597169010ae5
Nina Janson <janson.nin@gmail.com>
2022-05-10 11:43:07 +0200

    initial commit

--- script.js
+++ script.js
@@ -5,6 +5,29 @@ const header = document.querySelector("header");
 const body = document.querySelector("body");
 const colorRgb = document.querySelector(".colorRgb");
 const colorHex = document.querySelector(".colorHex");
+const button = document.querySelector("button");
+
+button.addEventListener("click", randomColor);
+
+function randomColor() {
+  fetch("https://dummy-apis.netlify.app/api/color")
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      }
+    })
+    .then((data) => {
+      red.value = data.rgb.r;
+      green.value = data.rgb.g;
+      blue.value = data.rgb.b;
+      changeBackgroundColor();
+      /*colorHex.innerText = data.color;
+      body.style.backgroundColor = data.color;
+      colorRgb.innerText =
+        "rgb(" + data.rgb.r + "," + data.rgb.g + "," + data.rgb.b + ")";
+      */
+    });
+}
 
 function valueToHex(c) {
   let hex = Number(c).toString(16);
